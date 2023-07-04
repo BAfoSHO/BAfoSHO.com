b5acb228e77b661f89b62e15f194c118e1fe92e8
BA <bafosho@gmail.com>
2023-07-03 21:39:28 -0500

    Update matrixBackground.js

--- src/matrixBackground.js
+++ src/matrixBackground.js
@@ -14,22 +14,34 @@ window.addEventListener('load', function(){
     for(var x = 0; x < columns; x++)
         drops[x] = 1; 
 
-    function draw() {
-        ctx.fillStyle = "rgba(0, 0, 0, 0.04)";
-        ctx.fillRect(0, 0, c.width, c.height);
+   //drawing the characters
+    function draw()
+    {
+    //Black BG for the canvas
+    //translucent BG to show trail
+    ctx.fillStyle = "rgba(0, 0, 0, 0.04)";
+    ctx.fillRect(0, 0, c.width, c.height);
 
-        ctx.fillStyle = "#f4427d";
-        ctx.font = font_size + "px arial";
-        for(var i = 0; i < drops.length; i++) {
-            var text = matrix[Math.floor(Math.random()*matrix.length)];
-            ctx.fillText(text, i*font_size, drops[i]*font_size);
+    ctx.fillStyle = "rgba(255, 0, 0, 0.5)"; // changed to opaque red color
+    ctx.font = font_size + "px arial";
+    //looping over drops
+    for(var i = 0; i < drops.length; i++)
+    {
+        //a random chinese character to print
+        var text = matrix[Math.floor(Math.random()*matrix.length)];
+        //x = i*font_size, y = value of drops[i]*font_size
+        ctx.fillText(text, i*font_size, drops[i]*font_size);
 
-            if(drops[i]*font_size > c.height && Math.random() > 0.975)
-                drops[i] = 0;
+        //sending the drop back to the top randomly after it has crossed the screen
+        //adding a randomness to the reset to make the drops scattered on the Y axis
+        if(drops[i]*font_size > c.height && Math.random() > 0.975)
+            drops[i] = 0;
 
-            drops[i]++;
+        //incrementing Y coordinate
+        drops[i]++;
         }
     }
+
 
     setInterval(draw, 60);
 });
