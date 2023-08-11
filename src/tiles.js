586d6fa368c230b2088e7de10ed430774b738ac1
BA <bafosho@gmail.com>
2023-08-11 17:52:49 -0500

    update 1.0.3

--- src/tiles.js
+++ src/tiles.js
@@ -1,27 +1,12 @@
-// Get the grid container element
-const gridContainer = document.getElementById('gridContainer');
+document.addEventListener("DOMContentLoaded", function () {
+  const dismissButtons = document.querySelectorAll(".dismiss-btn");
 
-// Function to create a tile element
-function createTile() {
-  const tile = document.createElement('div');
-  tile.classList.add('tile');
-  
-  const dismissBtn = document.createElement('span');
-  dismissBtn.classList.add('dismiss-btn');
-  dismissBtn.innerHTML = 'X';
-  
-  dismissBtn.addEventListener('click', () => {
-    gridContainer.removeChild(tile);
+  dismissButtons.forEach((button) => {
+    button.addEventListener("click", function () {
+      const tile = this.closest(".tile");
+      if (tile) {
+        tile.remove();
+      }
+    });
   });
-  
-  tile.appendChild(dismissBtn);
-  
-  return tile;
-}
-
-// Add tiles to the grid container
-for (let i = 0; i < 12; i++) { // You can adjust the number of initial tiles
-  const tile = createTile();
-  gridContainer.appendChild(tile);
-}
-
+});
