4d0c383b827f845bcb8390886de8f343901fa7b5
banderson15 <banderson15@uiowa.edu>
2024-05-09 19:19:01 -0500

    small changes

--- src/darklightmode.js
+++
@@ -1,9 +0,0 @@
-// Toggle dark mode
-const toggleDarkMode = () => {
-    document.body.classList.toggle('dark-mode');
-  };
-  
-  // Add event listener to button
-  const button = document.querySelector('#dark-mode-button');
-  button.addEventListener('click', toggleDarkMode);
-  
